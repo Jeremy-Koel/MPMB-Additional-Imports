0e3135ab3851c069a054257da83dc69b39cde012
Jeremy-Koel <38506627+Jeremy-Koel@users.noreply.github.com>
2019-08-05 13:09:58 -0500

    Create t_2016325_steampunkEquipment.js

---
+++ Homebrew/t_2016325_steampunkEquipment.js
@@ -0,0 +1,142 @@
+var iFileName = "t_2016325_steampunkEquipment";
+RequiredSheetVersion(12.999);
+// This file adds content from Tribality's Shawn Ellsworth's Steampunk Equipment for D&D 5th Edition to MPMB's Character Record Sheet
+
+// Define the Source
+SourceList["T:SE"] = {
+		name : "Steampunk Equipment for D&D 5th Edition",
+		abbreviation :	"T:SE",
+		group : "Tribality",
+		url : "https://www.tribality.com/2016/03/25/steampunk-equipment-for-dd-5th-edition/",
+		date : "2016/03/25"
+};
+
+// Adds Gear
+GearList["compass"] = {
+	infoname : "Compass [100 gp]",
+	name : "Compass",
+	amount : "",
+	weight : 0.25
+};
+		
+GearList["goggles"] = {
+	infoname : "Goggles [1 gp]",
+	name : "Goggles",
+	amount : "",
+	weight : 0.5
+};
+	
+GearList["hat"] = {
+	infoname : "Hat [1 sp to 5 gp]",
+	name : "Hat",
+	amount : "",
+	weight : 0.5
+};
+		
+GearList["ink cartridge pen"] = {
+	infoname : "Ink Cartridge Pen [1 gp]",
+	name : "Ink Cartridge Pen",
+	amount : "",
+	weight : 0.001
+};
+		
+GearList["ink cartridge"] = {
+	infoname : "Ink Cartridge [5 sp]",
+	name : "Ink Cartridge",
+	amount : "",
+	weight : 0.0001
+};
+
+GearList["lighter"] = {
+	infoname : "Lighter [8 gp]",
+	name : "Lighter",
+	amount : "",
+	weight : 0.04
+};
+
+GearList["match"] = {
+	infoname : "Match (Box of 40) [4 gp]",
+	name : "Match",
+	amount : "40",
+	weight : 0.009
+};
+
+GearList["parasol (silk)"] = {
+	infoname : "Parasol (Silk) [5 gp]",
+	name : "Parasol (Silk)",
+	amount : "",
+	weight : 0.04
+};
+
+GearList["pocket watch"] = {
+	infoname : "Pocket Watch [5 gp to 20 gp]",
+	name : "Pocket Watch",
+	amount : "",
+	weight : 1.4
+};
+
+GearList["photographic camera"] = {
+	infoname : "Photographic Camera [50 gp]",
+	name : "Photographic Camera",
+	amount : "",
+	weight : 4
+};
+
+GearList["photo plate (1 sheet)"] = {
+	infoname : "Photo Plate (1 sheet) [1 gp]",
+	name : "Photo Plate",
+	amount : "",
+	weight : 2
+};
+
+GearList["smokestick"] = {
+	infoname : "Smokestick [14 gp]",
+	name : "Smokestick",
+	amount : "",
+	weight : 0.5
+};
+
+GearList["steamer trunk (large)"] = {
+	infoname : "Steamer Trunk (Large) [5 gp]",
+	name : "Steamer Trunk (Large)",
+	amount : "",
+	weight : 20
+};
+
+GearList["tool/utility belt"] = {
+	infoname : "Tool/Utility Belt [2 gp]",
+	name : "Tool/Utility Belt",
+	amount : "",
+	weight : 2
+};	
+
+GearList["wristwatch"] = {
+	infoname : "Wristwatch [5 gp]",
+	name : "Wristwatch",
+	amount : "",
+	weight : 2.3
+};
+
+ToolsList["chemist’s supplies"] = {
+		infoname : "Chemist’s supplies [50 gp]",
+		name : "Chemist’s supplies",
+		amount : "",
+		weight : 8,
+		type : "artisan's tools"
+};
+
+ToolsList["investigator’s supplies"] = {
+		infoname : "Investigator’s supplies [50 gp]",
+		name : "Investigator’s supplies",
+		amount : "",
+		weight : 8,
+		type : "artisan's tools"
+};
+
+ToolsList["pharmacist’s supplies"] = {
+		infoname : "Pharmacist’s supplies [50 gp]",
+		name : "Pharmacist’s supplies",
+		amount : "",
+		weight : 8,
+		type : "artisan's tools"
+};
